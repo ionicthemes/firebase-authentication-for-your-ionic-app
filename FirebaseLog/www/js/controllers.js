7f2682c10c4074bf817ff58a47b6291f4771f4fa
Gonzalo Di Giovanni <gonza.digiovanni@gmail.com>
2016-06-03 18:13:11 -0300

    se refactorea el codigo

--- FirebaseLog/www/js/controllers.js
+++ FirebaseLog/www/js/controllers.js
@@ -30,6 +30,32 @@ angular.module('starter.controllers', [])
   $scope.logout = function(){
     ref.unauth();
   }
+
+  $scope.fbLogin = function(){
+    if(ionic.Platform.isWebView()){
+      $cordovaFacebook.login(["public_profile", "email"]).then(function(success){
+        console.log(success);
+        ref.authWithOAuthToken("facebook", success.authResponse.accessToken, function(error, authData) {
+          if (error) {
+            console.log('Firebase login failed!', error);
+          } else {
+            console.log('Authenticated successfully with payload:', authData);
+          }
+        });
+      }, function(error){
+        console.log(error);
+      });
+    }
+    else {
+      ref.authWithOAuthPopup("facebook", function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+        }
+      });
+    }
+  };
 })
 
 .controller('SignUpCtrl', function($scope, $stateParams) {
